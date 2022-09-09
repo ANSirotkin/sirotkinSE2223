2ca72a6ec0388b95b0f1bc3b9eb2e64429e97010
ANSirotkin <ansirotkin@icloud.com>
2022-09-09 12:26:01 -0400

    Create branchingDemo.js
    created branchingDemo.js

---
+++ branchingDemo.js
@@ -0,0 +1,14 @@
+//filename:     branchingDemo.js
+//Author:       S. Sirotkin
+//Objective:    Demonstrate how to use GitHub branches
+
+
+const months = ['january', 'february', 'march', 'april', 
+                'may', 'june', 'july', 'august', 'september', 
+                'october', 'november', 'december'];
+
+const d = new Date();
+
+let month = months[d.getMonth()];
+
+console.log('The date is: ' + d);
