77a0fd5792db16740f9f7cae8425393eb52d2e97
ANSirotkin <ansirotkin@icloud.com>
2022-11-07 14:15:19 -0500

    linechart

--- js examples/chartjs/js/linechart.js
+++ js examples/chartjs/js/linechart.js
@@ -1,100 +1,124 @@
-//Graph CSV data using chart.js
+new (class {
+	constructor() {
+		this.parseCSV()
+			.then(this.getData)
+			.then(this.createChart);
+	}
 
-const xYears = [];
-const yTemps = [];
+	async parseCSV() {
+		const response = await fetch("../data/test.csv");
+		const data = await response.text();
+		const lines = data.split("\n");
 
-//Parse CSV data into arrays
+		return lines.slice(1, lines.length - 1);
+	}
 
-async function getData(){
-    const response = await fetch("../data/test.csv");
-    const data = await response.text();
+	async getData(lines) {
+		const xYears = [],
+			yTemps = [],
+			yTempsNH = [],
+			yTempsSH = [];
 
-    const table = data.split("\n").slice(1);
+		const pushTemp = (array, temp) => array.push(parseFloat(temp) + 14);
 
-    table.forEach(row => {
-        const columns = row.split(",");
-        const year = columns[0];
-        xYears.push(year);
+		lines.forEach((line) => {
+			const [year, globalTempDeviation, nhTemp, shTemp] = line.split(",");
+			xYears.push(year);
 
-        const temp = columns[1];
-        yTemps.push(temp);
+			pushTemp(yTemps, globalTempDeviation);
+			pushTemp(yTempsNH, nhTemp);
+			pushTemp(yTempsSH, shTemp);
+		});
 
-        const nhTemp = columns[2];
+		return { xYears, yTemps, yTempsNH, yTempsSH };
+	}
 
-        const shTemp = columns[3];  
-    })
-}
-
-async function createChart(){
-    await getData();
-
-    const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,                   // Re-size based on screen size
-        scales: {                           // x & y axes display options
-            x: {
-                title: {
-                    display: true,
-                    text: 'x-Axis Title',
-                    font: {
-                        size: 20
-                    },
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'y-Axis Title',
-                    font: {
-                        size: 20
-                    },
-                }
-            }
-        },
-        plugins: {                          // title and legend display options
-            title: {
-                display: true,
-                text: 'Custom Chart Title',
-                font: {
-                    size: 24
-                },
-                padding: {
-                    top: 10,
-                    bottom: 30
-                }
-            },
-            legend: {
-                position: 'bottom'
-            }
-        }
-    }
-});
-}
-
-createChart();
+	createChart({ xYears, yTemps, yTempsNH, yTempsSH }) {
+		new Chart(document.getElementById('myChart'), {
+			type: 'line',
+			data: {
+				labels: xYears,
+				datasets: [
+					{
+						label: 'Global Temp. Deviations',
+						data: yTemps,
+						// 'rgba(54, 162, 235, 0.2)',
+						// 'rgba(255, 206, 86, 0.2)',
+						// 'rgba(75, 192, 192, 0.2)',
+						// 'rgba(153, 102, 255, 0.2)',
+						// 'rgba(255, 159, 64, 0.2)'
+						backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+						// 'rgba(54, 162, 235, 1)',
+						// 'rgba(255, 206, 86, 1)',
+						// 'rgba(75, 192, 192, 1)',
+						// 'rgba(153, 102, 255, 1)',
+						// 'rgba(255, 159, 64, 1)'
+						borderColor: ['rgba(255, 99, 132, 1)'],
+						borderWidth: 1
+					},
+					{
+						label: 'Northern Hemisphere Temps.',
+						data: yTempsNH,
+						backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+						borderColor: ['rgba(54, 162, 235, 1)'],
+						borderWidth: 1
+					},
+					{
+						label: 'Southern Hemisphere Temps.',
+						data: yTempsSH,
+						backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+						borderColor: ['rgba(255, 206, 86, 1)'],
+						borderWidth: 1
+					}
+				]
+			},
+			options: {
+				responsive: true,                   // Re-size based on screen size
+				scales: {                           // x & y axes display options
+					x: {
+						title: {
+							display: true,
+							text: 'Year',
+							font: {
+								size: 20
+							},
+						},
+						ticks: {
+							callback(val, index) {
+								return index % 5 == 0 ? this.getLabelForValue(val) : '';
+							},
+						},
+					},
+					y: {
+						title: {
+							display: true,
+							text: 'Global Mean Temperatures (C°)',
+							font: {
+								size: 20
+							},
+						},
+						ticks: {
+							maxTicksLimit: yTemps.length / 20,
+						}
+					}
+				},
+				plugins: {                          // title and legend display options
+					title: {
+						display: true,
+						text: 'Combined Land-Surface Air and Sea-Surface Water Temperature',
+						font: {
+							size: 24
+						},
+						padding: {
+							top: 10,
+							bottom: 30
+						}
+					},
+					legend: {
+						position: 'bottom'
+					}
+				}
+			}
+		});
+	}
+})();
